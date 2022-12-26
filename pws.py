b438e075555fea364767d1c633d99afe0dc3b430
Eric Snyder <guideon72@hotmail.com>
2022-12-26 12:37:20 -0800

    Updated pws.py to display titles on page based on Pandas dataframe; updated .gitignore to ignore vscode directory

--- pws.py
+++ pws.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(page_title="Title for my page", layout="wide")
 
@@ -46,4 +47,14 @@ st.write(
     "_____________________________________________________________________________________________"
 )
 
-# st.markdown('<p class="font">Guess the object Names</p>', unsafe_allow_html=True)
+df = pandas.read_csv("data/data.csv", sep=";")
+
+col3, col4 = st.columns(2)
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
