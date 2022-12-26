1f41317314a6f72dcdde97dcd194f3638e8459c9
Eric Snyder <guideon72@hotmail.com>
2022-12-26 13:39:42 -0800

    Added title, description, image and links to page via pandas and a csv; updated photo.png to be me

--- pws.py
+++ pws.py
@@ -25,7 +25,7 @@ def display_centered_text(text: str, container=None):
 
 
 with col1:
-    st.image("./images/photo.png", width=700)
+    st.image("./images/photo.png", width=400)
 
 with col2:
     st.title("My name goes here, eventually")
@@ -49,12 +49,18 @@ st.write(
 
 df = pandas.read_csv("data/data.csv", sep=";")
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.1, 1.5])
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"], width=200)
+        st.write(f'[Source Code: ]({row["url"]})')
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"], width=200)
+        st.write(f'[Source Code: ]({row["url"]})')
